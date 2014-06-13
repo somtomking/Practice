acb9f040a7fbbd0cdc029374e38e46dd7b939711
somtomking <somtomking@163.com>
2014-06-13 17:40:27 +0800

    dddd

--- Source/Practice.Tools/Dynamics/XmlDynamicObject.cs
+++ Source/Practice.Tools/Dynamics/XmlDynamicObject.cs
@@ -9,6 +9,19 @@ namespace Practice.Tools.Dynamics
 {
     public class XmlDynamicObject : DynamicObject
     {
-        
+        public XmlDynamicObject()
+        {
+            
+            
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return base.TryGetMember(binder, out result);
+        }
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            return base.TrySetMember(binder, value);
+        }
     }
 }
