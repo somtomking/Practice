c20bf2e04a805cd534e69f1d4ea8ef86e826a521
somtomking <somtomking@163.com>
2014-06-13 17:08:35 +0800

    init

---
+++ Source/Practice.Mvc5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practice.Mvc5.MvcApplication" Language="C#" %>
---
+++ Source/Practice.Mvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practice.Mvc5.Startup))]
+namespace Practice.Mvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
