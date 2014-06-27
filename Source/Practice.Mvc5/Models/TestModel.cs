813fb97bf49b2e5d44f98aa253609127b9547efe
somtomking <somtomking@163.com>
2014-06-27 14:09:19 +0800

    dddd

--- Source/Practice.Mvc5/Models/TestModel.cs
+++ Source/Practice.Mvc5/Models/TestModel.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 namespace Practice.Mvc5.Models
 {
     public class TestModel
     {
-
+        [DisplayName("名称")]
+        
         public string Name { get; set; }
+        [DisplayName("数量")]
+        public int Quantity { get; set; }
+        [DisplayName("价格")]
+        public decimal Price { get; set; }
+        [DisplayName("是否激活")]
+        public bool IsActive { get; set; }
+        [DisplayName("描述")]
+        public string Desc { get; set; }
     }
 }
