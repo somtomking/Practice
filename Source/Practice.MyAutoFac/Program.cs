352ae8b9bc397ddec16b084075dbeed8c138e96f
somtomking <somtomking@163.com>
2014-06-28 19:36:24 +0800

    dasfads

--- Source/Practice.MyAutoFac/Program.cs
+++ Source/Practice.MyAutoFac/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Autofac;
 namespace Practice.MyAutoFac
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterType<NullLoger>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterAssemblyModules<
+            var c = builder.Build();
+
+            var nullObj = c.Resolve<INullObj>();
+            var nullLog = c.Resolve<ILoger>();
+
+            Console.WriteLine(Object.ReferenceEquals(nullLog, nullObj));
+
+            Console.Read();
+
         }
     }
 }
