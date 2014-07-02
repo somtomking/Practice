c58538e08730f13d553fbe7fb9bf72ffb2ebb3e9
somtomking <somtomking@163.com>
2014-07-02 10:08:38 +0800

    model

---
+++ Source/Practice.EmptyWeb/HttpModules/ViewModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice.EmptyWeb.HttpModules
+{
+    public class ViewModule : IHttpModule
+    {
+        public void Dispose()
+        {
+
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(context_BeginRequest);
+            context.EndRequest += new EventHandler(context_EndRequest);
+        }
+
+        void context_EndRequest(object sender, EventArgs e)
+        {
+            ((HttpApplication)sender).Context.Response.Write("<h1>请求处理结束后进入自定义Module</h1>");
+        }
+
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            ((HttpApplication)sender).Context.Response.Write("<h1>请求处理开始前进入自定义Module</h1><hr>");
+        }
+    }
+}
