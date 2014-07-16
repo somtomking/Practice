683b06b29364c2a807a3ff7e693351c43715f8c9
somtomking <somtomking@163.com>
2014-07-16 21:14:31 +0800

    json

--- Source/Practice.Mvc5/Controllers/TestController.cs
+++ Source/Practice.Mvc5/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Practice.Mvc5.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -23,5 +24,41 @@ namespace Practice.Mvc5.Controllers
         {
             return View();
         }
+
+        public ActionResult Json()
+        {
+            Dictionary<string, ProvinceModel> dic = new Dictionary<string, ProvinceModel>();
+            for (int loop = 0; loop < 3; loop++)
+            {
+                ProvinceModel pm = new ProvinceModel() { Id = "A" + loop, Name = loop.ToString() };
+
+                for (int loop3 = 0; loop3 < 2; loop3++)
+                {
+                    pm.Cities.Add(new CityModel { Id = "B" + loop3, Name = loop3.ToString() });
+                }
+
+
+                dic.Add("A" + loop, pm);
+
+            }
+            ViewBag.Json1 = Newtonsoft.Json.JsonConvert.SerializeObject(dic);
+
+            Dictionary<int, List<int>> dict2 = new Dictionary<int, List<int>>();
+            for (int loop = 0; loop < 5; loop++)
+            {
+                dict2.Add(loop, new List<int>());
+                for (int loop2 = 0; loop2 < 3; loop2++)
+                {
+                    dict2[loop].Add(loop2);
+                }
+            }
+
+            ViewBag.Json2 = Newtonsoft.Json.JsonConvert.SerializeObject(dict2);
+
+            return View();
+        }
     }
+
+
 }
+
--- Source/Practice.Mvc5/Models/ProvinceModel.cs
+++ Source/Practice.Mvc5/Models/ProvinceModel.cs
@@ -7,14 +7,14 @@ namespace Practice.Mvc5.Models
 {
     public class IdAndName
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
     }
     public class ProvinceModel : IdAndName
     {
         public ProvinceModel()
         {
-
+            this.Cities = new List<CityModel>();
         }
         public IList<CityModel> Cities { get; set; }
     }
