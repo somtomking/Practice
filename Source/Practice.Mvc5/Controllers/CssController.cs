3c031ceb4a4446d3c54218afaf2f91836be915c8
somtomking <somtomking@163.com>
2014-08-01 17:32:57 +0800

    dd

---
+++ Source/Practice.Mvc5/Controllers/CssController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice.Mvc5.Controllers
+{
+    public class CssController : Controller
+    {
+        //
+        // GET: /Css/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
