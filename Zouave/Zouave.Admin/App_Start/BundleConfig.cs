bb4dedc95f0affe7e184576e88babaaa3e74a426
somtomking <somtomking@163.com>
2014-08-03 11:50:56 +0800

    keyi

--- Zouave/Zouave.Admin/App_Start/BundleConfig.cs
+++ Zouave/Zouave.Admin/App_Start/BundleConfig.cs
@@ -8,43 +8,70 @@ namespace Zouave.Admin
         // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+           
 
-            //admin js
-            bundles.Add(new ScriptBundle("~/bundles/admin").Include(
-                                        "~/Scripts/unicorn/excanvas.min.js",
-                                        "~/Scripts/unicorn/jquery.min.js",
-                                        "~/Scripts/unicorn/jquery-ui.custom.js",
-                                        "~/Scripts/unicorn/jquery.sparkline.min.js",
-                                        "~/Scripts/unicorn/jquery.nicescroll.min.js",
-                                        "~/Scripts/unicorn/jquery.flot.min.js",
-                                        "~/Scripts/unicorn/jquery.flot.pie.min.js",
-                                        "~/Scripts/unicorn/jquery.flot.resize.min.js",
-                                        "~/Scripts/unicorn/jquery.icheck.min.js",
-                                        "~/Scripts/unicorn/jquery.wizard.js",
-                                        "~/Scripts/unicorn/jquery.validate.js",
-                                        "~/Scripts/unicorn/jquery.gritter.min.js",
-                                        "~/Scripts/unicorn/jquery.dataTables.min.js",
-                                        "~/Scripts/unicorn/jquery.masonry.min.js",
-                                        "~/Scripts/unicorn/jquery.sparkline.min.js",
-                                        "~/Scripts/unicorn/bootstrap.min.js",
-                                        "~/Scripts/unicorn/fullcalendar.min.js",
-                                        "~/Scripts/jquery.nicescroll.min.js",
-                                        "~/Scripts/unicorn/select2.min.js",
-                                        "~/Scripts/unicorn/bootbox.min.js",
-                                        "~/Scripts/unicorn/wysihtml5.js",
-                                        "~/Scripts/unicorn/bootstrap-wysihtml5.js",
-                                        "~/Scripts/unicorn/unicorn.js",
-                                        "~/Scripts/unicorn/unicorn.dashboard.js",
-                                        "~/Scripts/unicorn/unicorn.tables.js",
-                                        "~/Scripts/unicorn/unicorn.calendar.js",
-                                        "~/Scripts/unicorn/unicorn.charts.js",
-                                        "~/Scripts/unicorn/unicorn.chat.js",
-                                        "~/Scripts/unicorn/unicorn.form_validation.js",
-                                        "~/Scripts/unicorn/unicorn.wizard.js",
-                                        "~/Scripts/unicorn/unicorn.jui.js"
-                  ));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //             "~/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //       "~/Scripts/jquery.validate.js"
+            //       ));
+
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            //             "~/Content/unicorn/js/jquery-ui.custom.js",
+            //               "~/Content/unicorn/js/jquery.icheck.min.js"
+            //           ));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryplugins").Include(
+            //            "~/Content/unicorn/js/excanvas.min.js",
+            //           "~/Content/unicorn/js/jquery.flot.min.js",
+            //            "~/Content/unicorn/js/jquery.flot.pie.min.js",
+            //            "~/Content/unicorn/js/jquery.flot.resize.min.js",
+            //            "~/Content/unicorn/js/jquery.sparkline.min.js",      /**图表*/
+
+            //             "~/Content/unicorn/js/jquery.wizard.js",            /*向导*/
+
+            //             "~/Content/unicorn/js/jquery.nicescroll.min.js",     /*其他*/
+            //             "~/Content/unicorn/js/fullcalendar.min.js",
+            //             "~/Content/unicorn/js/jquery.gritter.min.js",
+            //             "~/Content/unicorn/js/jquery.dataTables.min.js",
+            //             "~/Content/unicorn/js/jquery.masonry.min.js",
+            //             "~/Content/unicorn/js/select2.min.js",
+            //             "~/Content/unicorn/js/bootbox.min.js",
+            //             "~/Content/unicorn/js/wysihtml5.js"
+
+            //          ));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //      "~/Scripts/bootstrap.js",
+            //      "~/Content/unicorn/js/bootstrap-wysihtml5.js",
+            //      "~/Scripts/respond.js"));
+
+
+            //// 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
+            //// 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+
+
+            ////admin js
+            //bundles.Add(new ScriptBundle("~/bundles/unicorn").Include(
+            //                             "~/Content/unicorn/js/unicorn.js",
+            //                             "~/Content/unicorn/js/unicorn.dashboard.js",
+            //                            "~/Content/unicorn/js/unicorn.tables.js",
+            //                            "~/Content/unicorn/js/unicorn.calendar.js",
+            //                            "~/Content/unicorn/js/unicorn.charts.js",
+            //                            "~/Content/unicorn/js/unicorn.chat.js",
+            //                            "~/Content/unicorn/js/unicorn.form_validation.js",
+            //                            "~/Content/unicorn/js/unicorn.wizard.js",
+            //                            "~/Content/unicorn/js/unicorn.jui.js"
+            //      ));
 
             bundles.Add(new StyleBundle("~/Content/admin").Include(
+                   "~/Content/unicorn/css/jquery-ui.css",
                  "~/Content/unicorn/css/bootstrap.min.css",
                  "~/Content/unicorn/css/font-awesome.css",
                  "~/Content/unicorn/css/unicorn.css",
@@ -54,7 +81,6 @@ namespace Zouave.Admin
                  "~/Content/unicorn/css/datepicker.css",
                  "~/Content/unicorn/css/icheck/flat/blue.css",
                  "~/Content/unicorn/css/select2.css",
-                 "~/Content/unicorn/css/jquery-ui.css",
                  "~/Content/unicorn/css/jquery.gritter.css"
                  ));
 
--- Zouave/Zouave.Admin/Controllers/HomeController.cs
+++ Zouave/Zouave.Admin/Controllers/HomeController.cs
@@ -12,18 +12,8 @@ namespace Zouave.Admin.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Test()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
