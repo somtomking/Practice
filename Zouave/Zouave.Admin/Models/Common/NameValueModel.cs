00076ddbdc2f5b3446a8630631ba50307a1fa695
somtomking <somtomking@163.com>
2014-09-01 19:59:46 +0800

    东东

---
+++ Zouave/Zouave.Admin/Models/Common/NameValueModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zouave.Admin.Models.Common
+{
+    public class NameValueModel : ViewModelBase
+    {
+        public virtual string Name { get; set; }
+        public virtual dynamic Value { get; set; }
+    }
+}
