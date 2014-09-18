0a7f9346be358255167286a66f0409233674fe42
somtomking <somtomking@163.com>
2014-09-18 14:25:21 +0800

    修改

---
+++ Source/Practice.CoeveryMvc/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Practice.CoeveryMvc
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Source/Practice.Mvc5/Controllers/JsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice.Mvc5.Controllers
+{
+    public class JsController : Controller
+    {
+        //
+        // GET: /Js/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
