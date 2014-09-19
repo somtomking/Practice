07ed56be27092ca2658640e85576320d2abf2277
somtomking <somtomking@163.com>
2014-09-19 15:56:12 +0800

    模板

---
+++ Source/Practice.Mvc5/Controllers/JsTemplController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice.Mvc5.Controllers
+{
+    public class JsTemplController : Controller
+    {
+        //
+        // GET: /JsTempl/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
