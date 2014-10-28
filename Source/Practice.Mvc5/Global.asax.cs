2f29ee2941b2998992ba86f8ca4d8ca53f1cfdff
somtomking <somtomking@163.com>
2014-10-28 14:01:32 +0800

    autofac

--- Source/Practice.Mvc5/Global.asax.cs
+++ Source/Practice.Mvc5/Global.asax.cs
@@ -8,19 +8,35 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+using Autofac;
+using Autofac.Integration.Mvc;
+using System.Reflection;
+using Practice.Mvc5.Services;
 namespace Practice.Mvc5
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static IContainer Container { get; private set; }
         protected void Application_Start()
         {
+           
+
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             FluentValidationConfig.ConfigureContainer();
+
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterType<AutoFacTestService>().As<IAutoFacTestService>().InstancePerLifetimeScope();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsImplementedInterfaces();
+
+            var container = builder.Build();
+            MvcApplication.Container = container;
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
 
 
---
+++ Source/Practice.Mvc5/Services/IAutoFacTestService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Mvc5.Services
+{
+    public interface IAutoFacTestService
+    {
+        int Do();
+    }
+
+    public class AutoFacTestService : IAutoFacTestService
+    {
+        public AutoFacTestService()
+        {
+
+        }
+        private int Count = 0;
+        public int Do()
+        {
+            Count++;
+            return Count;
+        }
+    }
+}
