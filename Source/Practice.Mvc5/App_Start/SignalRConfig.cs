a65f34ffa09c0c700bcde01387555f7c67ac74ce
somtomking <somtomking@163.com>
2014-12-09 11:02:38 +0800

    signalr

---
+++ Source/Practice.Mvc5/App_Start/SignalRConfig.cs
@@ -0,0 +1,17 @@
+﻿using Practice.Mvc5.Infrastructure.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Practice.Mvc5.App_Start
+{
+    public class SignalRConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapConnection<MyConnection>("echo", "echo/{*operation}");
+        }
+    }
+}
---
+++ Source/Practice.Mvc5/Infrastructure/SignalR/MyConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Practice.Mvc5.Infrastructure.SignalR
+{
+    /// <summary>
+    /// 客户端连接服务端
+    /// </summary>
+    public class MyConnection : PersistentConnection
+    {
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            return Connection.Send(connectionId, "Welcome!");
+        }
+
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Broadcast(data);
+        }
+    }
+}
