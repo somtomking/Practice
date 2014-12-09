49070dfe52341c09e06ae69c42bea340acf5ccea
somtomking <somtomking@163.com>
2014-12-09 16:23:45 +0800

    ddd

---
+++ Source/Practice.Mvc5/Controllers/SignalRController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice.Mvc5.Controllers
+{
+    public class SignalR2Controller : Controller
+    {
+        //
+        // GET: /SignalR/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
